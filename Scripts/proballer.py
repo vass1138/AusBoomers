0dfd0dce7c27bb180e186c50bab9d1306eaa244d
vass1138 <66472742+vass1138@users.noreply.github.com>
2021-08-08 22:41:48 +1000

    Updated parser for final team
    Proballers removed div id from tables.  Needs to be tested further.

--- Scripts/proballer.py
+++ Scripts/proballer.py
@@ -31,12 +31,38 @@ def get_primary_table(soup,name):
     return df
 
 def get_secondary_tables(soup,name):
+
+    # valid IDs Feb 2021
     divs = soup.find_all('div', id=['list-playoff','list-european-competitions','list-international-competitions'])
 
     df = pd.DataFrame()
 
     for div in divs:
         source = div['id']
+
+        # find table within div
+        tables = div.find_all('table', class_='table')
+        print(f"{source}, {len(tables)}")
+
+        # always table zero in list
+        dt = pd.read_html(str(tables[0]))[0]
+        dt['Source'] = source
+
+        df = df.append(dt,ignore_index=True)
+
+    df['Name'] = name
+
+    return df
+
+def get_secondary_tables_by_class(soup,name):
+
+    # valid IDs Feb 2021
+    divs = soup.find_all('div', class_=['select-tab-1','select-tab-2','select-tab-3','select-tab-4','select-tab-5'])
+
+    df = pd.DataFrame()
+
+    for div in divs:
+        source = div['class'].replace('select-tab-content ','')
 
         # find table within div
         tables = div.find_all('table', class_='table')
