115bc9c3804ab6c98d042080f4845f9af82d3bdf
vass1138 <66472742+vass1138@users.noreply.github.com>
2021-08-08 21:32:22 +1000

    Restructured project

---
+++ Scripts/proballer.py
@@ -0,0 +1,53 @@
+# proballer.py
+import pandas as pd
+import sys
+
+# this is a pointer to the module object instance itself.
+this = sys.modules[__name__]
+
+# we can explicitly make assignments on it 
+this.url_template = "https://www.proballers.com/basketball/player/{PROBALLER_ID}/{PLAYER}"
+
+def get_player_url(id,name):
+    id_str = str(id)
+    name = name.lower().replace(' ','-')
+    url = this.url_template
+    url = url.replace('{PROBALLER_ID}',id_str)
+    url = url.replace('{PLAYER}',name)
+    print(f"--- {url} ---")
+    return url
+
+def get_primary_table(soup,name):
+    source = 'list-regular'
+
+    tables = soup.find_all('table', class_='table')
+    print(f"{source}, {len(tables)}")
+
+    df = pd.read_html(str(tables[0]))[0]
+
+    df['Source'] = source
+    df['Name'] = name
+
+    return df
+
+def get_secondary_tables(soup,name):
+    divs = soup.find_all('div', id=['list-playoff','list-european-competitions','list-international-competitions'])
+
+    df = pd.DataFrame()
+
+    for div in divs:
+        source = div['id']
+
+        # find table within div
+        tables = div.find_all('table', class_='table')
+        print(f"{source}, {len(tables)}")
+
+        # always table zero in list
+        dt = pd.read_html(str(tables[0]))[0]
+        dt['Source'] = source
+
+        df = df.append(dt,ignore_index=True)
+
+    df['Name'] = name
+
+    return df
